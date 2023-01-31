bcdd85891e97ba9dc74e45223ccd6743639d9c27
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-31 19:05:01 +0200

    add lazy state in APP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,23 @@ import { Form } from './Form/Form';
 import { useState, useEffect } from 'react';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
+  const [contacts, setContacts] = useState(() => {
     const storageContacts = localStorage.getItem('contacts');
     if (storageContacts) {
       const parsedContacts = JSON.parse(storageContacts);
-      setContacts(parsedContacts);
+      return parsedContacts;
+    } else {
+      return [
+        { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+        { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+        { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+        { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+      ];
     }
-  }, []);
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
